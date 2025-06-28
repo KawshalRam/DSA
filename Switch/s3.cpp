7c88e55891d83bda008023b07881b1d3177280c1
KawshalRam <kawshalram@gmail.com>
2025-06-28 18:45:14 +0530

    s3 commit

---
+++ Switch/s3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the amoount"<<endl;
+    cin>>n;
+    int y=n;
+
+    switch()
+    {
+        case  : 
+        {
+            y=n/100;
+            cout<<y<<endl;
+        }
+    }
+}
