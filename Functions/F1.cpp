2300c0b99402c2d874bf26b618f4b7aff0f77e04
KawshalRam <kawshalram@gmail.com>
2025-06-28 18:24:55 +0530

    Best Approach

--- Functions/F1.cpp
+++ Functions/F1.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
-int powerofAandB(int a,int b){
+int powerofAandB(){
+    int a, b;
+    cin>>a>>b;
     int ans=1;
     for (int i=1;i<=b;i++)
     {
         ans=ans*a;
     }
+    cout<<ans<<endl;
     return ans;
     }
 int main()
 {
     //pow(a,b)
-    int a,b;
+    /*int a,b;
     cin>>a>>b;
     int answer=powerofAandB(a,b);
     cout<<answer;
-    return 0;
+    return 0;*/
+    powerofAandB();
+    powerofAandB();
 }
