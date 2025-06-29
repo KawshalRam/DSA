d9a7e7a2f4f0ac1cb8dd4fa5713461db6d671a0f
KawshalRam <kawshalram@gmail.com>
2025-06-30 00:10:03 +0530

    Modified Reverse

--- Arrays/Reverse.cpp
+++ Arrays/Reverse.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int Reverse(int arr[],int size)
+void Reverse(int arr[],int size)
 {
-    for (int i=size;i>=0;i--)
+    cout<<"enter the array"<<endl;
+    for (int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i=size-1;i>=0;i--)
     {
         cout<<arr[i]<<" ";
     }
@@ -14,11 +19,6 @@ int main()
     int arr[100],size;
     cout<<"enter the size of array"<<endl;
     cin>>size;
-    cout<<"enter the array"<<endl;
-    for (int i=0;i<size;i++)
-    {
-        cin>>arr[i];
-    }
     Reverse(arr,size);
     return 0;
 }
